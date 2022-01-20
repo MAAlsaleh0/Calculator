5e9bdb27fbf156c72a48c7e60bcd557dcc9af5b1
Mohammed Alsaleh <malsalehh0@gmail.com>
2022-01-20 10:44:04 -0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by m on 17/06/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by m on 17/06/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
