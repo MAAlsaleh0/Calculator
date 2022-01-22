66daf8e05cdce0c1b8e79120bf647bca909c4013
Mohammed Alsaleh <malsalehh0@gmail.com>
2022-01-22 08:55:49 -0800

    .

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -28,16 +28,27 @@ enum CalcButton: String {
     case percent = "%"
     case negative = "-/+"
     
-    var buttonColor: Color {
+    var buttonColor : Color {
         switch self {
-        case .add, .subtract, .mutliply, .divide, .equal:
-            return .orange
-        case .clear, .negative, .percent:
-            return Color(.lightGray)
+        case .clear , .equal , .divide , .mutliply , .subtract , .add :
+            return Color("Yellow")
         default:
-            return Color(UIColor(red: 55/255.0, green: 55/255.0, blue: 55/255.0, alpha: 1))
+            return Color("ButtonColor")
         }
     }
+    
+    var BackGroundColor: Color {
+        switch self {
+        case .add, .subtract, .mutliply, .divide, .equal:
+            return Color("BGButtonColor")
+        default:
+            return Color.clear
+        }
+    }
+}
+
+enum Operation {
+    case add, subtract, multiply, divide, none
 }
 
 struct ContentView: View {
@@ -49,6 +60,11 @@ struct ContentView: View {
         [.zero, .decimal, .equal],
     ]
     @State var ToggleBool = false
+    @State var value = "0"
+    @State var value1 = "0"
+    @State var Numbers = ""
+    @State var runningNumber = 0.0
+    @State var currentOperation: Operation = .none
     var body: some View {
         ZStack {
             Color("BackGround")
@@ -59,25 +75,37 @@ struct ContentView: View {
                     .toggleStyle(CustomToggle())
                     .frame(maxWidth:.infinity , alignment:.leading)
                     .padding()
-                Text("0")
-                    .font(.system(size:52))
-                    .bold()
+                Text(value1)
+                    .font(.system(size:100))
+                    .fontWeight(.medium)
                     .foregroundColor(Color("Yellow"))
                     .frame(maxWidth:.infinity , alignment:.trailing)
-                    .padding()
+                    .padding(.horizontal)
+                if Numbers != "" {
+                    Text(Numbers)
+                        .font(.title2)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color("Yellow"))
+                        .frame(maxWidth:.infinity , alignment:.trailing)
+                        .padding(.horizontal)
+                }
+                Spacer()
                 ForEach(self.buttons , id: \.self) { row in
                     HStack{
                         ForEach(row , id: \.self) { button in
                             Button(action:{
-                                
-                            }) {
-                                if button.rawValue == "=" {
-                                    Text(button.rawValue)
-                                        .font(.title)
-                                        .foregroundColor(Color("Yellow"))
-                                        .frame(width: 200 , height:50).background(Color("Gray").opacity(0.3).cornerRadius(15))
+                                withAnimation(.spring()) {
+                                    self.didTap(button: button)
                                 }
-                                
+                            }) {
+                                Text(button.rawValue)
+                                    .font(.largeTitle)
+                                    .fontWeight(.medium)
+                                    .frame(width: self.buttonWidth(item: button),
+                                           height: self.buttonHeight())
+                                    .background(button.BackGroundColor.opacity(0.1))
+                                    .foregroundColor(button.buttonColor)
+                                    .cornerRadius(self.buttonWidth(item: button)/2)
                             }
                         }
                     }
@@ -85,7 +113,85 @@ struct ContentView: View {
             }
         }.preferredColorScheme(ToggleBool ? .dark : .light)
     }
+    func didTap(button: CalcButton) {
+        
+        switch button {
+            
+        case .add, .subtract, .mutliply, .divide, .equal :
+            if button == .add {
+                self.Numbers += (" \(button.rawValue.description) ")
+                self.currentOperation = .add
+                self.runningNumber = Double(self.value) ?? 0.0
+            } else if button == .subtract {
+                self.Numbers += (" \(button.rawValue.description) ")
+                self.currentOperation = .subtract
+                self.runningNumber = Double(self.value) ?? 0.0
+            } else if button == .mutliply {
+                self.Numbers += (" \(button.rawValue.description) ")
+                self.currentOperation = .multiply
+                self.runningNumber = Double(self.value) ?? 0.0
+            } else if button == .divide {
+                self.Numbers += (" \(button.rawValue.description) ")
+                self.currentOperation = .divide
+                self.runningNumber = Double(self.value) ?? 0.0
+            } else if button == .equal {
+                
+                let runningValue = self.runningNumber
+                let currentValue = Double(self.value) ?? 0.0
+                switch self.currentOperation {
+                case .add: self.value1 = "\(runningValue + currentValue)"
+                case .subtract: self.value1 = "\(runningValue - currentValue)"
+                case .multiply: self.value1 = "\(runningValue * currentValue)"
+                case .divide: self.value1 = "\(runningValue / currentValue)"
+                case  .none :
+                    break
+                }
+            }
+            
+            if button != .equal {
+                self.value = "0"
+                
+            }
+        case .clear:
+            self.value = "0"
+            self.value1 = "0"
+            self.Numbers = ""
+        case .decimal :
+            self.value += "."
+            self.Numbers += "."
+        case .negative , .percent :
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+                
+            } else {
+                self.value = "\(self.value)\(number)"
+            }
+            if self.value == "0" {
+                self.Numbers = ""
+            }
+            if self.Numbers != "" {
+                self.Numbers += "\(number)"
+            } else {
+                self.Numbers = value
+            }
+        }
+    }
+    
+    func buttonWidth(item: CalcButton) -> CGFloat {
+        if item == .equal {
+            return ((UIScreen.main.bounds.width - (4*12)) / 4) * 2
+        }
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
+    
+    func buttonHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5*12)) / 4
+    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
